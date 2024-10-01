608474ca87a42fde94800556d37fdbd7c29f0f15
Umbrases <zhenua27@gmail.com>
2024-10-01 16:05:46 +0500

    Contact add

--- app/Model/Contact.php
+++ app/Model/Contact.php
@@ -3,196 +3,11 @@
 namespace App\Model;
 
 use App\Service\ContactService;
+use App\Controller\LeadController;
+
 
 class Contact
 {
-    private ?int $id;
-    private ?string $city;
-    private ?string $name;
-    private ?string $secondName;
-    private ?string $lastName;
-    private ?string $phoneValueType;
-    private $phoneValue;
-    private $birthdate;
-    private $address;
-    private $emailValue;
-    private $emailValueType;
-    private $passportSerial;
-    private $passportNumber;
-    private $dateIssue;
-    private $departmentCode;
-    private $issuer;
-    private $leadId;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
-
-    public function setId(?int $id): void
-    {
-        $this->id = $id;
-    }
-    public function getCity(): ?string
-    {
-        return $this->city;
-    }
-
-    public function setCity(?string $city): void
-    {
-        $this->city = $city;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(?string $name): void
-    {
-        $this->name = $name;
-    }
-
-    public function getSecondName(): ?string
-    {
-        return $this->secondName;
-    }
-
-    public function setSecondName(?string $secondName): void
-    {
-        $this->secondName = $secondName;
-    }
-
-    public function getLastName(): ?string
-    {
-        return $this->lastName;
-    }
-
-    public function setLastName(?string $lastName): void
-    {
-        $this->lastName = $lastName;
-    }
-
-    public function getPhoneValueType(): ?string
-    {
-        return $this->phoneValueType;
-    }
-
-    public function setPhoneValueType(?string $phoneValueType): void
-    {
-        $this->phoneValueType = $phoneValueType;
-    }
-
-    public function getPhoneValue()
-    {
-        return $this->phoneValue;
-    }
-
-    public function setPhoneValue($phoneValue): void
-    {
-        $this->phoneValue = $phoneValue;
-    }
-
-    public function getBirthdate()
-    {
-        return $this->birthdate;
-    }
-
-    public function setBirthdate($birthdate): void
-    {
-        $this->birthdate = $birthdate;
-    }
-
-    public function getAddress()
-    {
-        return $this->address;
-    }
-
-    public function setAddress($address): void
-    {
-        $this->address = $address;
-    }
-
-    public function getEmailValue()
-    {
-        return $this->emailValue;
-    }
-
-    public function setEmailValue($emailValue): void
-    {
-        $this->emailValue = $emailValue;
-    }
-
-    public function getEmailValueType()
-    {
-        return $this->emailValueType;
-    }
-
-    public function setEmailValueType($emailValueType): void
-    {
-        $this->emailValueType = $emailValueType;
-    }
-
-    public function getPassportSerial()
-    {
-        return $this->passportSerial;
-    }
-
-    public function setPassportSerial($passportSerial): void
-    {
-        $this->passportSerial = $passportSerial;
-    }
-
-    public function getPassportNumber()
-    {
-        return $this->passportNumber;
-    }
-
-    public function setPassportNumber($passportNumber): void
-    {
-        $this->passportNumber = $passportNumber;
-    }
-
-    public function getDateIssue()
-    {
-        return $this->dateIssue;
-    }
-
-    public function setDateIssue($dateIssue): void
-    {
-        $this->dateIssue = $dateIssue;
-    }
-
-    public function getDepartmentCode()
-    {
-        return $this->departmentCode;
-    }
-
-    public function setDepartmentCode($departmentCode): void
-    {
-        $this->departmentCode = $departmentCode;
-    }
-
-    public function getIssuer()
-    {
-        return $this->issuer;
-    }
-
-    public function setIssuer($issuer): void
-    {
-        $this->issuer = $issuer;
-    }
-
-    public function getLeadId()
-    {
-        return $this->leadId;
-    }
-
-    public function setLeadId($leadId): void
-    {
-        $this->leadId = $leadId;
-    }
-
     public function saveContact($classFrom, $classBefore, $contactId)
     {
         $safeMySQL = new SafeMySQL;
@@ -201,21 +16,23 @@ class Contact
         $sqlBeforeId = $safeMySQL
             ->getRow("SELECT `contact_box` FROM det_contact where contact_cloud = ?i", (int)$contactId)['contact_box'];
         $sqlContact = "INSERT INTO det_contact SET contact_box = ?i, contact_cloud = ?i";
-        $sqlUpdateContact = "UPDATE det_contact SET contact_box = ?i WHERE contact_cloud = ?i";
 
         //Вывод контакта
         $contact = $contactService->getContact($classFrom, $contactId, $sqlBeforeId, $classBefore);
 
-        if (!empty($contact->getLeadId())) {
+        if (!empty($contact['LEAD_ID'])) {
             $sqlLead = $safeMySQL
-                ->getRow("SELECT `lead_box` FROM det_lead where lead_cloud = ?i", $contact->getLeadId());
+                ->getRow("SELECT `lead_box` FROM det_lead where lead_cloud = ?i", $contact['LEAD_ID'])['lead_box'];
+            if(empty($sqlLead)) {
+                $leadController = new LeadController();
+                $leadController->create($contact['LEAD_ID']);
+            }
         }
 
         if (empty($sqlBeforeId)) {
             $contactService->setContact($contact, $classBefore, $sqlContact);
         } else {
-            writeToLog($contact);
-            $contactService->updateContact($contact, $classBefore, $sqlUpdateContact, $sqlBeforeId);
+            $contactService->updateContact($contact, $classBefore, $sqlBeforeId);
         }
     }
 
--- app/Service/ContactService.php
+++ app/Service/ContactService.php
@@ -14,153 +14,193 @@ class ContactService
             'crm.contact.get', [
                 'ID' => $contactId,
             ]);
+        $responseAdressCloud = QueryHelper::getQuery($classFrom, 'crm.address.list',[
+            'filter' => [
+                'ENTITY_TYPE_ID' => 3,
+                'ENTITY_ID' => $contactId
+            ]
+        ])['result'];
 
         if (empty($sqlBeforeId)) {
-            return $this->buildContactFromResponseArray($responseArrayCloud['result']);
+            return $this->buildContactFromResponseArray($responseArrayCloud['result'], $responseAdressCloud);
         } else {
             $comparisonResult = $this->comparisonContact($responseArrayCloud['result'], $classBefore, $sqlBeforeId);
-            return $this->buildContactFromResponseArray($comparisonResult);
+            return $this->buildContactFromResponseArray($comparisonResult, $responseAdressCloud);
         }
     }
 
-    public function buildContactFromResponseArray(array $responseArrayCloud) :Contact
+    public function buildContactFromResponseArray(array $responseArrayCloud, $responseAdressCloud)
     {
-        $contact = new Contact();
+        $contact = [];
+        foreach ($responseArrayCloud as $key => $value) {
+            switch ($key) {
+                case 'ID' :
+                    $contact['ID'] = $value;
+                    break;
+                case 'UF_CRM_629A1B699D519' :
+                    $contact['UF_CRM_629A1B699D519'] = $value;
+                    break;
+                case 'NAME' :
+                    $contact['NAME'] = $value;
+                    break;
+                case 'SECOND_NAME' :
+                    $contact['SECOND_NAME'] = $value;
+                    break;
+                case 'LAST_NAME' :
+                    $contact['LAST_NAME'] = $value;
+                    break;
+                case 'PHONE' :
+                    $contact['PHONE'] = $value;
+                    break;
+                case 'EMAIL' :
+                    $contact['EMAIL'] = $value;
+                    break;
+                case 'BIRTHDATE' :
+                    $contact['BIRTHDATE'] = $value;
+                    break;
+                case 'ADDRESS' :
+                    $contact['ADDRESS'] = $responseAdressCloud;
+                    break;
+                case 'UF_CRM_5D53E5846CE99' :
+                    $contact['UF_CRM_5D53E5846CE99'] = $value;
+                    break;
+                case 'UF_CRM_1624004832' :
+                    $contact['UF_CRM_1624004832'] = $value;
+                    writeToLog($value);
+                    break;
+                case 'UF_CRM_1663670733026' :
+                    $contact['UF_CRM_1663670733026'] = $value;
+                    break;
+                case 'UF_CRM_62CD365DB2DED' :
+                    $contact['UF_CRM_62CD365DB2DED'] = $value;
+                    break;
+                case 'UF_CRM_62CD365D51F74' :
+                    $contact['UF_CRM_62CD365D51F74'] = $value;
+                    break;
+                case 'UF_CRM_62CD365DECFE7' :
+                    $contact['UF_CRM_62CD365DECFE7'] = $value;
+                    break;
+                case 'COMMENTS' :
+                    $contact['COMMENTS'] = $value;
+                    break;
+                case 'UF_CRM_629F51D7AE750' :
+                    $contact['UF_CRM_629F51D7AE750'] = $value;
+                    break;
+                case 'UF_CRM_629F51D7F1D30' :
+                    $contact['UF_CRM_629F51D7F1D30'] = $value;
+                    break;
+                case 'UF_CRM_629F51D834666' :
+                    $contact['UF_CRM_629F51D834666'] = $value;
+                    break;
+                case 'UF_CRM_629F51D85F1A7' :
+                    $contact['UF_CRM_629F51D85F1A7'] = $value;
+                    break;
+                case 'UF_CRM_629F51D88AC70' :
+                    $contact['UF_CRM_629F51D88AC70'] = $value;
+                    break;
+                case 'UF_CRM_1720601597' :
+                    $contact['UF_CRM_1720601597'] = $responseArrayCloud['ID'];
+                    break;
+                case 'LEAD_ID' :
+                    $contact['LEAD_ID'] = $value;
+                    break;
+            }
+        }
+        $contact['UF_CRM_1721830931'] = 'https://stopzaym.bitrix24.ru/crm/contact/details/'.$responseArrayCloud['ID'].'/?any=details%2F31674%2F';
+        $contact['UF_CRM_1722586545'] = 'https://stopzaym.bitrix24.ru/crm/contact/details/'.$responseArrayCloud['ID'].'/?any=details%2F31674%2F';
 
-        $contact->setId($responseArrayCloud['ID']);
-        array_key_exists('NAME', $responseArrayCloud) ??
-            $contact->setName($responseArrayCloud['NAME']);
-        array_key_exists('UF_CRM_629A1B699D519', $responseArrayCloud) ??
-            $contact->setCity($responseArrayCloud['UF_CRM_629A1B699D519']);
-        array_key_exists('SECOND_NAME', $responseArrayCloud) ??
-            $contact->setLastName($responseArrayCloud['SECOND_NAME']);
-        array_key_exists('LAST_NAME', $responseArrayCloud) ??
-            $contact->setSecondName($responseArrayCloud['LAST_NAME']);
-        !empty($responseArrayCloud['PHONE'][0]['VALUE']) ??
-            $contact->setPhoneValue($responseArrayCloud['PHONE'][0]['VALUE']);
-        !empty($responseArrayCloud['PHONE'][0]['VALUE_TYPE']) ??
-            $contact->setPhoneValueType($responseArrayCloud['PHONE'][0]['VALUE_TYPE']);
-        array_key_exists('BIRTHDATE', $responseArrayCloud) ??
-            $contact->setBirthdate($responseArrayCloud['BIRTHDATE']);
-        array_key_exists('ADDRESS', $responseArrayCloud) ??
-            $contact->setAddress($responseArrayCloud['ADDRESS']);
-        !empty($responseArrayCloud['EMAIL'][0]['VALUE']) ??
-            $contact->setEmailValue($responseArrayCloud['EMAIL'][0]['VALUE']);
-        !empty($responseArrayCloud['EMAIL'][0]['VALUE_TYPE']) ??
-            $contact->setEmailValueType($responseArrayCloud['EMAIL'][0]['VALUE_TYPE']);
-        array_key_exists('UF_CRM_629F51D7AE750', $responseArrayCloud) ??
-            $contact->setPassportSerial($responseArrayCloud['UF_CRM_629F51D7AE750']);
-        array_key_exists('UF_CRM_629F51D7F1D30', $responseArrayCloud) ??
-            $contact->setPassportNumber($responseArrayCloud['UF_CRM_629F51D7F1D30']);
-        array_key_exists('UF_CRM_629F51D834666', $responseArrayCloud) ??
-            $contact->setDateIssue($responseArrayCloud['UF_CRM_629F51D834666']);
-        array_key_exists('UF_CRM_629F51D88AC70', $responseArrayCloud) ??
-            $contact->setDepartmentCode($responseArrayCloud['UF_CRM_629F51D88AC70']);
-        array_key_exists('LEAD_ID', $responseArrayCloud) ??
-            $contact->setLeadId($responseArrayCloud['LEAD_ID']);
-writeToLog($contact);
         return $contact;
     }
 
-    public function setContact(Contact $contact, $classBefore, $sqlContact)
+    public function setContact($contact, $classBefore, $sqlContact)
     {
         $safeMySQL = new SafeMySQL;
 
+        $fields = [];
+        foreach ($contact as $key => $value) {
+            if (!empty($value)) $fields[$key] = $value;
+        }
+
+        writeToLog($contact);
         $methodQuery = QueryHelper::getQuery($classBefore, 'crm.contact.add', [
-            'fields' => [
-                'NAME' => $contact->getName(),
-                'SECOND_NAME' => $contact->getSecondName(),
-                'LAST_NAME' => $contact->getLastName(),
-                'PHONE' => [[
-                    'VALUE' => $contact->getPhoneValue(),
-                    'VALUE_TYPE' => $contact->getPhoneValueType(),
-                ]],
-                'BIRTHDATE' => $contact->getBirthdate(),
-                'ADDRESS' => $contact->getAddress(),
-                'EMAIL' => [[
-                    'VALUE' => $contact->getEmailValue(),
-                    'VALUE_TYPE' => $contact->getEmailValueType(),
-                ]],
-                'UF_CRM_629A1B699D519' => $contact->getCity(),
-                'UF_CRM_629F51D7AE750' => $contact->getPassportSerial(),
-                'UF_CRM_629F51D7F1D30' => $contact->getPassportNumber(),
-                'UF_CRM_629F51D834666' => $contact->getDateIssue(),
-                'UF_CRM_629F51D88AC70' => $contact->getDepartmentCode(),
-                'UF_CRM_629F51D85F1A7' => $contact->getIssuer(),
-                'LEAD_ID' => $contact->getLeadId(),
-            ]
+            'fields' => $fields
         ]);
 
-        $safeMySQL->query($sqlContact, $methodQuery['result'], $contact->getId());
+        foreach ($contact['ADDRESS'] as $keyAddress => $valueAddress) {
+            (QueryHelper::getQuery($classBefore, 'crm.address.add', [
+                'fields' => [
+                    'TYPE_ID' => 1,
+                    'ENTITY_TYPE_ID' => 3,
+                    'ENTITY_ID' => $methodQuery['result'],
+                    'PROVINCE' => $contact['ADDRESS'][$keyAddress]['PROVINCE'],
+                    'COUNTRY' => $contact['ADDRESS'][$keyAddress]['COUNTRY'],
+                    'CITY' => $contact['ADDRESS'][$keyAddress]['CITY'],
+                    'ADDRESS_1' => $contact['ADDRESS'][$keyAddress]['ADDRESS_1'],
+                    ]
+            ]));
+        }
+        $safeMySQL->query($sqlContact, $methodQuery['result'], $contact['ID']);
     }
 
     public function comparisonContact($responseArrayCloud, $classBefore, $sqlBeforeId)
     {
         $responseArrayBox = QueryHelper::getQuery($classBefore,
-            'crm.contact.list', [
-                'filter' =>[
+            'crm.contact.get', [
                     'ID' => $sqlBeforeId,
-                ],
-                'select' => [
-                    'ID',
-                    'UF_CRM_629A1B699D519',
-                    'NAME',
-                    'SECOND_NAME',
-                    'LAST_NAME',
-                    'PHONE',
-                    'BIRTHDATE',
-                    'ADDRESS',
-                    'EMAIL',
-                    'UF_CRM_629F51D7AE750',
-                    'UF_CRM_629F51D7F1D30',
-                    'UF_CRM_629F51D834666',
-                    'UF_CRM_629F51D85F1A7',
-                    'UF_CRM_629F51D88AC70',
-                    'LEAD_ID',
-                ]
             ]);
 
+        if (!array_key_exists('PHONE', $responseArrayBox['result'])) $responseArrayBox['result']['PHONE'][0] = null;
+        if (!array_key_exists('EMAIL', $responseArrayBox['result'])) $responseArrayBox['result']['EMAIL'][0] = null;
+
+        $response = [];
+        $responsePhone = [];
+        $responseEmail = [];
         foreach ($responseArrayCloud as $keyCloud => $valueCloud) {
-            foreach ($responseArrayBox['result'][0] as $keyBox => $valueBox) {
-                if ($keyCloud != $keyBox) continue;
-                if ($valueBox != $valueCloud) $response[$keyCloud] = $valueCloud;
+            foreach ($responseArrayBox['result'] as $keyBox => $valueBox) {
+                if ($keyCloud === $keyBox
+                    && $valueBox != $valueCloud
+                    && $keyCloud != 'EMAIL'
+                    && $keyCloud != 'PHONE') $response[$keyCloud] = $valueCloud;
+
+                if ($keyCloud === $keyBox && $keyCloud == 'PHONE'){
+                    foreach ($valueCloud as $keyPhoneCloud => $valuePhoneCloud){
+                        foreach ($valueBox as $keyPhoneBox => $valuePhoneBox) {
+                            if ($valuePhoneCloud['VALUE'] != $valuePhoneBox['VALUE'] || empty($valuePhoneBox)) {
+                                $responsePhone[$keyPhoneCloud]['VALUE'] = $valuePhoneCloud['VALUE'];
+                                $responsePhone[$keyPhoneCloud]['VALUE_TYPE'] = $valuePhoneCloud['VALUE_TYPE'];
+                            }
+                        }
+                    }
+                }
+                if ($keyCloud === $keyBox && $keyCloud == 'EMAIL'){
+                    foreach ($valueCloud as $keyEmailCloud => $valueEmailCloud){
+                        foreach ($valueBox as $keyPhoneBox => $valueEmailBox) {
+                            if ($valueEmailCloud['VALUE'] != $valueEmailBox['VALUE']) {
+                                $responseEmail[$keyEmailCloud]['VALUE'] = $valueEmailCloud['VALUE'];
+                                $responseEmail[$keyEmailCloud]['VALUE_TYPE'] = $valueEmailCloud['VALUE_TYPE'];
+                            }
+                        }
+                    }
+                }
             }
         }
+        $response['PHONE'] = $responsePhone;
+        $response['EMAIL'] = $responseEmail;
+
         return $response;
     }
 
-    public function updateContact(Contact $contact, $classBefore, $sqlUpdateContact, $sqlBeforeId)
+    public function updateContact($contact, $classBefore, $sqlBeforeId)
     {
-        $safeMySQL = new SafeMySQL;
-
-        $methodQuery = QueryHelper::getQuery($classBefore, 'crm.contact.update', [
+        $fields = [];
+        foreach ($contact as $key => $value) {
+            if (!empty($value)) $fields[$key] = $value;
+        }
+writeToLog($contact);
+        QueryHelper::getQuery($classBefore, 'crm.contact.update', [
             'id' => $sqlBeforeId,
-            'fields' => [
-                !empty($contact->getName()) ?? 'NAME' => $contact->getName(),
-                !empty($contact->getSecondName()) ?? 'SECOND_NAME' => $contact->getSecondName(),
-                !empty($contact->getLastName()) ?? 'LAST_NAME' => $contact->getLastName(),
-                !empty($contact->getPhoneValue()) ?? 'PHONE' => [[
-                    'VALUE' => $contact->getPhoneValue(),
-                    'VALUE_TYPE' => $contact->getPhoneValueType(),
-                ]],
-                !empty($contact->getBirthdate()) ?? 'BIRTHDATE' => $contact->getBirthdate(),
-                !empty($contact->getAddress()) ?? 'ADDRESS' => $contact->getAddress(),
-                !empty($contact->getEmailValue()) ?? 'EMAIL' => [[
-                    'VALUE' => $contact->getEmailValue(),
-                    'VALUE_TYPE' => $contact->getEmailValueType(),
-                ]],
-                !empty($contact->getCity()) ?? 'UF_CRM_629A1B699D519' => $contact->getCity(),
-                !empty($contact->getPassportSerial()) ?? 'UF_CRM_629F51D7AE750' => $contact->getPassportSerial(),
-                !empty($contact->getPassportNumber()) ?? 'UF_CRM_629F51D7F1D30' => $contact->getPassportNumber(),
-                !empty($contact->getDateIssue()) ?? 'UF_CRM_629F51D834666' => $contact->getDateIssue(),
-                !empty($contact->getDepartmentCode()) ?? 'UF_CRM_629F51D88AC70' => $contact->getDepartmentCode(),
-                !empty($contact->getIssuer()) ?? 'UF_CRM_629F51D85F1A7' => $contact->getIssuer(),
-                !empty($contact->getLeadId()) ?? 'LEAD_ID' => $contact->getLeadId(),
-            ]
+            'fields' => $fields
         ]);
-writeToLog($sqlBeforeId);
-writeToLog($methodQuery);
-        $safeMySQL->query($sqlUpdateContact, $methodQuery['result'], $contact->getId());
     }
 
 }
